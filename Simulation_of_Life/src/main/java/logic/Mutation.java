8eb44f8b2032196fc826eae4883a1af47bb8ffb9
xMOROx <zajdel0202@gmail.com>
2022-12-16 23:49:07 +0100

    Add mutation system

---
+++ Simulation_of_Life/src/main/java/logic/Mutation.java
@@ -0,0 +1,36 @@
+package logic;
+
+import java.util.List;
+import java.util.Random;
+
+public class Mutation
+{
+    private DefaultConfiguration config;
+    private final Random random = new Random();
+    public Mutation(DefaultConfiguration config) {
+        this.config = config;
+    }
+
+    public static class DefaultConfiguration {
+        public int numberOfMinimumGenes = 1;
+        public int numberOfMaximumGenes = 2;
+    }
+
+    public Genome normalMutation(Genome genome) {
+        List<Integer> genes = genome.getGenes();
+        int numberOfMutations = random.nextInt(this.config.numberOfMinimumGenes, this.config.numberOfMaximumGenes);
+        for (int i = 0; i < numberOfMutations; i++) {
+            int geneIndex = random.nextInt(genes.size());
+            int geneValue = random.nextInt(8);
+            genes.set(geneIndex, geneValue);
+        }
+        return new Genome(genes);
+    }
+
+    public Genome controlMutation(Genome genome) {
+        List<Integer> genes = genome.getGenes();
+        int numberOfMutations = random.nextInt(this.config.numberOfMinimumGenes, this.config.numberOfMaximumGenes);
+
+        return new Genome(genes);
+    }
+}
