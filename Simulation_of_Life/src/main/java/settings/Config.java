d2dc7e17f90e74b20db0d3a677f41f3dde6b0fbc
xMOROx <zajdel0202@gmail.com>
2022-12-20 21:42:32 +0100

    add config class

---
+++ Simulation_of_Life/src/main/java/settings/Config.java
@@ -0,0 +1,31 @@
+package settings;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Config {
+    public WorldConfig world;
+    public SpawnerConfig[] spawners;
+
+    private static Gson g = new Gson();
+    static Config fromFile(String path) throws FileNotFoundException {
+        var fileReader = new FileReader(path);
+        return g.fromJson(fileReader, Config.class);
+    }
+
+    public static class WorldConfig {
+        public int width = 200;
+        public int height = 200;
+    }
+
+    public static class SpawnerConfig {
+        public String type;
+        public JsonElement config;
+    }
+}
